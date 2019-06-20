0a2f3a025ffff0abc5134a2b9325445d461a18b3
gokul <gokulgangadharanp@gmail.com>
2019-06-20 14:19:19 +0530

    added changes to the all.js

--- all.js
+++ all.js
@@ -3,11 +3,11 @@ function add(a, b){
 }
 
 function sub(a, b){
-    return a - b;
+    return a - logb;
 }
 
 function mul(a, b){
-    return a * b;
+    return a * mod(b);
 }
 
 function div(a, b){
--- gokul2.js
+++ gokul2.js
@@ -1,5 +1,5 @@
 function name(){
-    
+    console.log("poor joke alert")
     return "Poor Joke: "+ " what did the rock say to the king? /n"+
     "I rock, you rule"
 }
