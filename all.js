ac8ace7cf007ba64a1ab559681e0fe4a41c042ed
prdpshnkr <prdpfotos@gmail.com>
2019-06-20 13:53:16 +0530

    changes to all

--- all.js
+++ all.js
@@ -1,15 +1,16 @@
-function add(a, b){
+function add(a, b) {
+    console.log("changes by pradeep")
     return a + b;
 }
 
-function sub(a, b){
-    return a - b;
-}
-
-function mul(a, b){
+function sub(a, b) {
     return a * b;
 }
 
-function div(a, b){
+function mul(a, b) {
     return a / b;
+}
+
+function div(a, b) {
+    return a - b;
 }
